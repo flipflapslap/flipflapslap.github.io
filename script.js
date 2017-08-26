c80ac4cf5da5fe2885ad39de88b3708c5983ca07
jayce <jaycecanale@gmail.com>
2017-08-25 23:18:56 -0400

    Added Reset Button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	var gridSquare = $('.gridSquare');
 	var container = $('.container');
-	var lineBreak = ("<br/>");
 	for (var i = 1; i < 256; i++){
 		if (i % 16 == 0){
 			(container).append("<br/>");
@@ -10,4 +9,16 @@ $(document).ready(function(){
 			gridSquare.clone().appendTo(container);
 		}
 	}
+});
+
+$(document).ready(function(){
+	$('.gridSquare').hover(function(){
+		$(this).css("background-color", "black");
+	})
+});
+
+$(document).ready(function(){
+	$("#button").click(function(){
+		$(".gridSquare").css("background-color", "pink");
+	});
 });
