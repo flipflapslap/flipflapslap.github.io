6e6ba2ddffa24204389ccd5fda7f5c95c9277fcf
jayce <jaycecanale@gmail.com>
2017-08-28 20:13:37 -0400

    Misc

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-var numberOfSquares = 5;
-var area = numberOfSquares * numberOfSquares;
-
 $(document).ready(function(){
+	initGrid(3);
+	hover();
+	newGrid();
+});
+
+function initGrid(numberOfSquares){
 	var gridSquare = $('.gridSquare');
 	var container = $('.container');
-	var gridSquareHeight = 500 / numberOfSquares;
-	var gridSquareWidth = 500 / numberOfSquares;
+	var gridSquareSize = 500 / numberOfSquares;
+	var area = numberOfSquares * numberOfSquares;
 	
-	gridSquare.height(gridSquareHeight);
-	gridSquare.width(gridSquareWidth);
+	gridSquare.height(gridSquareSize);
+	gridSquare.width(gridSquareSize);
 
 	for (var i = 1; i < area; i++){
-		if (i % numberOfSquares == 0){
+		gridSquare.clone().appendTo(container);
+	}
+		/*if (i % numberOfSquares == 0){
 			(container).append("<br/>");
 			gridSquare.clone().appendTo(container);
 		} else {
 			gridSquare.clone().appendTo(container);
 		}
-	}
-});
+	}*/
+}
 
-$(document).ready(function(){
+function newGrid(){
+	$("#button").click(function(){
+		var input = prompt("Enter number of Squares");
+		$('.container').empty();
+		initGrid(input);
+	});
+} 
+
+function hover(){
 	$('.gridSquare').hover(function(){
 		$(this).css("background-color", "#9d9dfb");
 	})
-});
+}
 
-$(document).ready(function(){
-	$("#button").click(function(){
-		$(".gridSquare").css("background-color", "pink");
-		numberOfSquares = prompt("Enter number of Squares");
-		area = numberOfSquares * numberOfSquares;
-	});
-});
+
+
 
